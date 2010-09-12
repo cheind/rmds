78845be3ea6e396a078b39dea560346588cac250
Christoph Heindl <christoph.heindl@gmail.com>
2010-09-12 18:24:21 +0200

    fixed loading paths due to new directory layout

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+#
+# rmds - Ruby Multidimensional Scaling Library
+# Copyright (c) Christoph Heindl, 2010
+# http://code.google.com/p/mdsl/
+#
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'test/unit'
+require 'mds'
